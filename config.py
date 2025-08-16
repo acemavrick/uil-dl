5509703496b9f76ba0852ad15f2d12f6ebb66ef6
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-08-16 14:13:41 -0500

    Initialized a basic website and trimmed unnecessary files

--- config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from platformdirs import user_data_path
-
-# central app data directory (side-effect free; no prints)
-data_path: Path = user_data_path(
-    appname="uil-dl", appauthor="acemavrick", ensure_exists=True
-)
-
-
